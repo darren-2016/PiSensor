a3b7040d92a3293ef2ba78bafa8587f76c80b333
darren-2016 <darren.draper@gpsengine.net>
2017-08-18 06:36:31 +0000

    MonitorClimate funciton added plus tidy-up

--- sensor.py
+++ sensor.py
@@ -1,53 +1,62 @@
 ######################################################################
 # PiSensor
 #
-# Get the temperate, humidity and pressure readings
-# Send the readings to the generic receiver rdev.gpsbe.net port 5013 - 
+# Climate monitoring application.  
+# Monitoring temperature, humidity and pressure.
+# Send the readings to the generic receiver rdev.gpsbe.net port 5013 
 # The generic receiver used for testing new receivers.
 # http requests port 5013
 #
-#
 # Use HTTP Requests to send formatted data to port 5013 at rdev.gpsbe.net
 #
+# TODO: Implement a task sheduling system, such as cron
 
 
 #import socket
 import json
 import urllib2
+import requests
 
 # Define raspPi as True if running on a Raspberry Pi, otherwise False.
-raspPi = False
+raspPi = True
 
-class Sensor:
-    temperature = 0;
-    pressure = 0;
-    humidity = 0;
-
-
+# Define webaccess as True if access web services, otherwise False.
+webaccess = True
 
 if raspPi == True:
     from sense_hat import SenseHat
 
 else:
+    # Emulate SenseHat
     class SenseHat():
         # Get Temperature
         def get_temperature(self):
-            print "Get Temperature"
+            print "**** Emulate Get Temperature "
             return 1
 
        # Get Pressure
         def get_pressure(self):
-            print "Get Pressure"
+            print "**** Emulate Get Pressure"
             return 2
 
         # Get Humidity
         def get_humidity(self):
-            print "Get Humidity"
+            print "**** Emulate Get Humidity"
             return 3
 
         # Display message
         def show_message(self, messageString):
-            print messageString
+            print "**** Emulate LED Matrix Display: " + messageString
+
+
+############################################################
+# Class:       Sensor
+# Description: Object for storing the temperature, pressure 
+#              and humidity readings.
+class Sensor:
+    temperature = 0;
+    pressure = 0;
+    humidity = 0;
 
 
 ############################################################
@@ -61,48 +70,40 @@ def sendToReceiver(sensorData):
     print "Humidity:    " + str(sensorData.humidity)
     print "Pressure:    " + str(sensorData.pressure)
 
-    receiverUrl = "http://rdev.gpsbe.net"
-    port = "5013"
-    fullUrl = receiverUrl + ":" + port
+    if webaccess == True:
+        receiverUrl = "http://rdev.gpsbe.net"
+        port = "5013"
+        fullUrl = receiverUrl + ":" + port
+        
+        data = json.dumps({'Temperature' : sensorData.temperature, 'Humidity': sensorData.humidity, 'Pressure': sensorData.pressure})
+        print data
+
+        print "Full URL = " + fullUrl
+
+        # Send to receiver using requests functionality
+        try:
+            r = requests.post(fullUrl, data, timeout=5)
+        except requests.Timeout:
+            print "Timeout!"
     
-    #fullUrl = "http://dev-messaging-service.appspot.com"
-
-    data = json.dumps({'Temperature' : sensorData.temperature, 'Humidity': sensorData.humidity, 'Pressure': sensorData.pressure})
-    print data
-
-    print "Full URL = " + fullUrl
-
-    u = urllib2.urlopen(fullUrl, data)
-    #response = u.getcode()
-    #print response
+        #print r.status_code
     
-    u.close()
+        #u = urllib2.urlopen(fullUrl, data)
+        #response = u.getcode()
+        #print response
+    
+        #u.close()
     print "Done!"
 
 
-
+# Global initialisations
 sense = SenseHat()
-
 sensor = Sensor()
 
-sense.show_message("Sensor")
-
-#while True:
-#    t = sense.get_temperature()
-#    p = sense.get_pressure()
-#    h = sense.get_humidity()
-
-#    msg = "Temperature = {0}, Pressure = {1}, Humidity = {2}".format(t,p,h)
-
-#    sense.show_message(msg) #, scroll_speed=0.05)
-
-
 ############################################################
-# Main routine
-# 
-def main():
-    print "Pi Sensor"
-
+# Function:    monitorClimate
+# Description:
+def monitorClimate():
     sensor.temperature = sense.get_temperature()
     sensor.pressure = sense.get_pressure()
     sensor.humidity = sense.get_humidity()
@@ -111,13 +112,25 @@ def main():
 
     msg = "Temperature = {0}, Pressure = {1}, Humidity = {2}".format(sensor.temperature, sensor.pressure, sensor.humidity)
     
-    sense.show_message(msg) #, scroll_speed=0.05)
+    #sense.show_message(msg) #, scroll_speed=0.05)
+
+             
+############################################################
+# Main routine
+# 
+def main():
+    print "Pi Sensor"
+
+    sense.show_message("Climate Sensor")
+
+    monitorClimate()
+
 
     #s = socket.socket()
 
 
 
-
+# Main entry point
 if __name__ == '__main__':
     main()
 
